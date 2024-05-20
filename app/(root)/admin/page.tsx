66468d6322dd2f7400215704e98a97f49ab52c1f
angelstchavez <angelstchavez@gmail.com>
2024-05-19 20:22:00 -0500

    feat: update dashboard

--- app/(root)/admin/page.tsx
+++ app/(root)/admin/page.tsx
@@ -8,22 +8,20 @@ const AdminPage = () => {
     hour12: true,
   });
   const date = now.toLocaleDateString("es-CO", {
+    weekday: "long",
     day: "2-digit",
     month: "long",
     year: "numeric",
   });
   return (
-    <section className="flex size-full flex-col gap-10 text-white">
-      <div className="h-[300px] w-full rounded-[20px] bg-travely-400 shadow-md">
+    <section className="flex size-full flex-col gap-10 text-zinc-900 py-2">
+      <div className="h-[200px] w-full rounded-xl bg-white shadow-md">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 px-5 py-8">
-          <h2 className="bg-travely-500 max-w-[270px] rounded py-2 text-center text-base font-normal">
-            Bienvenido
-          </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
-            <p>{date}</p>
+            <p>{date}.</p>
           </div>
-        </div>{" "}
+        </div>
       </div>
     </section>
   );

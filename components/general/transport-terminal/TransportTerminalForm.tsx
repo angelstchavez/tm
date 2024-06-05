fe750c641d8be378833fffde0019b6dddc2c6200
Juan Pineda <juanpineda3000@gmail.com>
2024-06-05 03:12:28 -0500

    add:FormularioTerminal

--- components/general/transport-terminal/TransportTerminalForm.tsx
+++ components/general/transport-terminal/TransportTerminalForm.tsx
@@ -1,11 +1,193 @@
+"use client";
+
+import ComboBox from "@/components/ui/combobox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import Section from "@/components/ui/Section";
 import CustomTitle from "@/components/utils/CustomTitle";
-import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import useFetch from "@/hooks/useFetch";
+import { CreateEntityDialog } from "@/components/api/CreateEntity";
+import { Button } from "@/components/ui/button";
+
+type FormData = z.infer<typeof FormSchema>;
+
+const FormSchema = z.object({
+  name: z.string().nonempty("Introduce un nombre valido."),
+  address: z.string().nonempty("Introduce una direccion."),
+  phoneNumber: z
+    .string()
+    .max(10, "El numero de telefono no puede ser mayor a 10 digitos")
+    .nonempty("Introduce numero de telefono."),
+  departamentoId: z.string().nonempty("Selecciona un departamento."),
+  cityId: z.string().nonempty("Selecciona un municipio."),
+  isActive: z.boolean(),
+  createdAt: z.date(), // Agregamos el campo createdAt
+});
+
+interface Departamentos {
+  id: string;
+  name: string;
+}
+
+interface Municipio extends Departamentos {}
 
 const TransportTerminalForm = () => {
+  const [formData, setFormData] = useState<Omit<
+    FormData,
+    "departamentoId"
+  > | null>(null);
+
+  const [selectedCity, setSelectedCity] = useState<string>("0");
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: zodResolver(FormSchema),
+    mode: "onBlur",
+    defaultValues: {
+      name: "",
+      address: "",
+      phoneNumber: "",
+      departamentoId: "",
+      cityId: "",
+      isActive: true,
+      createdAt: new Date(),
+    },
+  });
+
+  const onSubmit = (data: FormData) => {
+    const { departamentoId, ...rest } = data;
+    setFormData(rest);
+    setDialogOpen(true);
+  };
+
+  const handleError = (error: string) => console.log("Ocurrio un error");
+  const {
+    data: dataDepart,
+    hasError,
+    isLoading,
+  } = useFetch<Departamentos>("/deparment/get-all");
+
+  const opcionDepartamento = dataDepart.map((departamentos) => ({
+    value: departamentos.id,
+    label: departamentos.name,
+  }));
+
+  const watchDepartamentoId = watch("departamentoId");
+
+  useEffect(() => {
+    setSelectedCity(watchDepartamentoId);
+  }, [watchDepartamentoId]);
+
+  const {
+    data: dataCity,
+    hasError: hasErroCity,
+    isLoading: isLoadingCity,
+  } = useFetch<Municipio>(`/city/get-cities-by-deparment-id/${selectedCity}`);
+
+  const opcionMunicipios = dataCity.map((municipios) => ({
+    value: municipios.id,
+    label: municipios.name,
+  }));
+
+  const handleOnComplete = () => {
+    console.log("Entidad creada con éxito");
+    setDialogOpen(false);
+    setFormData(null);
+  };
+
   return (
     <Section>
-      <CustomTitle title={"Registrar terminal de transporte"}></CustomTitle>
+      <CustomTitle title={"Registrar terminal de transporte"} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid  grid-cols-1 lg:grid-cols-3 gap-x-3 gap-y-2">
+          <div>
+            <Label>Nombre</Label>
+            <Input id="name" placeholder="Nombre" {...register("name")} />
+            {errors.name && (
+              <span className="text-red-600 text-sm font-sans">
+                {errors.name.message}
+              </span>
+            )}
+          </div>
+
+          <div className="lg:col-span-2">
+            <Label>Direccion</Label>
+            <Input
+              id="address"
+              placeholder="Direccion"
+              {...register("address")}
+            />
+            {errors.address && (
+              <span className="text-red-600 text-sm font-sans">
+                {errors.address.message}
+              </span>
+            )}
+          </div>
+
+          <div>
+            <ComboBox
+              label="Departamento"
+              id="departamentoId"
+              options={opcionDepartamento}
+              register={register("departamentoId")}
+            />
+            <span className="text-red-600 text-sm font-sans">
+              {errors.departamentoId?.message}
+            </span>
+          </div>
+
+          <div>
+            <ComboBox
+              label="Ciudad"
+              id="cityId"
+              options={opcionMunicipios}
+              register={register("cityId")}
+              disabled={!watch("departamentoId")}
+            />
+            <span className="text-red-600 text-sm font-sans">
+              {errors.cityId?.message}
+            </span>
+          </div>
+
+          <div>
+            <Label>Numero de telefono</Label>
+            <Input
+              id="phoneNumber"
+              placeholder="Telefono"
+              {...register("phoneNumber")}
+            />
+            {errors.phoneNumber && (
+              <span className="text-red-600 text-sm font-sans">
+                {errors.phoneNumber.message}
+              </span>
+            )}
+          </div>
+        </div>
+        <div className="mt-4">
+          <CreateEntityDialog
+            entity="transport-terminal"
+            entityName={formData?.name ?? ""}
+            entityAttributes={formData ?? {}}
+            onComplete={handleOnComplete}
+            onError={handleError}
+            buttonComponent={
+              <Button variant={"travely"} disabled={!isValid} type="submit">
+                Crear
+              </Button>
+            }
+          />
+        </div>
+      </form>
     </Section>
   );
 };
--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,16 +1,14 @@
 import { useAuthToken } from "@/contexts/AuthContext";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
+type UseFetchState<T> = {
+  isLoading: boolean;
+  data: T[];
+  hasError: null | Error;
+};
 
-type useFetchState<T> = {
-    data:T[],
-    isLoading:boolean,
-    hasError: null | Error;
-}
-
-const useFetch = <T>() => {
-
-  const [fetchState, setFetchState] = useState<useFetchState<T>>({
+const useFetch = <T>(url: string) => {
+  const [fetchState, setFetchState] = useState<UseFetchState<T>>({
     data: [],
     isLoading: true,
     hasError: null,
@@ -18,45 +16,125 @@ const useFetch = <T>() => {
 
   const token = useAuthToken();
 
-  const fetchData = async (url:string) => {
-    try {
-      setFetchState((oldValue) => ({
-        ...oldValue,
-        isLoading: true,
-      }));
+  const getFetch = useCallback(
+    async (signal: AbortSignal) => {
+      try {
+        setFetchState((oldValue) => ({
+          ...oldValue,
+          isLoading: true,
+        }));
 
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}${url}`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          Accept: "application/json",
-        },
-      });
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}${url}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+            },
+            signal,
+          }
+        );
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-      const {data} = await response.json();
+        const { data } = await response.json();
 
-      setFetchState({
-        data,
-        isLoading:false,
-        hasError:null
-      })
-
-    } catch (error:any) {
         setFetchState({
-            data:[],
-            isLoading:false,
-            hasError: error.messaje
+          data,
+          isLoading: false,
+          hasError: null,
         });
-    }
-  };
+      } catch (error: any) {
+        setFetchState({
+          data: [],
+          isLoading: false,
+          hasError: error.message,
+        });
+      }
+    },
+    [token, url]
+  );
 
-  return {data: fetchState.data,
-          isLoading: fetchState.isLoading,
-          hasError: fetchState.hasError,
-          fetchData
-   };
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    if (token) {
+      getFetch(signal);
+    }
+
+    return () => {
+      controller.abort();
+    };
+  }, [token, url]); // Ejecutar el efecto cuando token cambie
+
+  return {
+    data: fetchState.data,
+    isLoading: fetchState.isLoading,
+    hasError: fetchState.hasError,
+  };
 };
+
+export default useFetch;
+
+// type useFetchState<T> = {
+//     data:T[],
+//     isLoading:boolean,
+//     hasError: null | Error;
+// }
+
+// export const useFetch = <T>() => {
+
+//   const [fetchState, setFetchState] = useState<useFetchState<T>>({
+//     data: [],
+//     isLoading: true,
+//     hasError: null,
+//   });
+
+//   const token = useAuthToken();
+
+//   const fetchData = async (url:string) => {
+//     try {
+//       setFetchState((oldValue) => ({
+//         ...oldValue,
+//         isLoading: true,
+//       }));
+
+//       const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}${url}`, {
+//         method: "GET",
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//           Accept: "application/json",
+//         },
+//       });
+
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
+
+//       const {data} = await response.json();
+
+//       setFetchState({
+//         data,
+//         isLoading:false,
+//         hasError:null
+//       })
+
+//     } catch (error:any) {
+//         setFetchState({
+//             data:[],
+//             isLoading:false,
+//             hasError: error.messaje
+//         });
+//     }
+//   };
+
+//   return {data: fetchState.data,
+//           isLoading: fetchState.isLoading,
+//           hasError: fetchState.hasError,
+//           fetchData
+//    };
+// };

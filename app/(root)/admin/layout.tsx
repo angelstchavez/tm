2eebf03815f578bec95eda3897b310b3c34757fd
Anibal Fuentes <anibaldfuentesprieto@gmail.com>
2024-06-06 18:31:18 -0500

    fix: refactor

--- app/(root)/admin/layout.tsx
+++ app/(root)/admin/layout.tsx
@@ -1,13 +1,16 @@
 'use client'
-
-
+import { ReactNode } from 'react'
 import Header from '@/components/utils/Header'
 import HeaderMobile from '@/components/utils/HeaderMobile'
 import MarginWidthWrapper from '@/components/utils/MarginWifthWrapper'
 import PageWrapper from '@/components/utils/PageWrapper'
 import SideNav from '@/components/utils/Sidenav'
 
-function Layout ({ children }: { children: React.ReactNode }) {
+type LayoutProps = Readonly<{
+  children: ReactNode
+}>
+
+function Layout ({ children }: LayoutProps) {
   return (
     <div>
       <div className='flex'>

44b3f7368227021cb7101429ab1bb4d2d0c2eb48
Anibal Fuentes <anibaldfuentesprieto@gmail.com>
2024-06-06 18:41:32 -0500

    fix: refactor

--- app/(root)/admin/registers/vehicles/page.tsx
+++ app/(root)/admin/registers/vehicles/page.tsx
@@ -1,45 +1,43 @@
-"use client";
+'use client'
 
-import BrandCounter from "@/components/general/brands/BrandCounter";
-import BrandForm from "@/components/general/brands/BrandForm";
-import BrandTable from "@/components/general/brands/BrandTable";
-import ModelCounter from "@/components/general/model/ModelCounter";
-import ModelForm from "@/components/general/model/ModelForm";
-import ModelTable from "@/components/general/model/ModelTable";
-import VehicleCounter from "@/components/general/vehicles/VehicleCounter";
-import VehicleForm from "@/components/general/vehicles/VehicleForm";
-import VehicleTable from "@/components/general/vehicles/VehicleTable";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { TabsContent } from "@radix-ui/react-tabs";
-import React from "react";
+import BrandCounter from '@/components/general/brands/BrandCounter'
+import BrandForm from '@/components/general/brands/BrandForm'
+import BrandTable from '@/components/general/brands/BrandTable'
+import ModelCounter from '@/components/general/model/ModelCounter'
+import ModelForm from '@/components/general/model/ModelForm'
+import ModelTable from '@/components/general/model/ModelTable'
+import VehicleCounter from '@/components/general/vehicles/VehicleCounter'
+import VehicleForm from '@/components/general/vehicles/VehicleForm'
+import VehicleTable from '@/components/general/vehicles/VehicleTable'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { TabsContent } from '@radix-ui/react-tabs'
+import React from 'react'
 
 const VehiclesPage: React.FC = () => {
   return (
-    <>
-      <Tabs defaultValue="vehicle">
-        <TabsList>
-          <TabsTrigger value="vehicle">Vehículos</TabsTrigger>
-          <TabsTrigger value="model">Modelos</TabsTrigger>
-          <TabsTrigger value="brand">Marcas</TabsTrigger>
-        </TabsList>
-        <TabsContent value="vehicle">
-          <VehicleCounter></VehicleCounter>
-          <VehicleForm></VehicleForm>
-          <VehicleTable></VehicleTable>
-        </TabsContent>
-        <TabsContent value="model">
-          <ModelCounter></ModelCounter>
-          <ModelForm></ModelForm>
-          <ModelTable></ModelTable>
-        </TabsContent>
-        <TabsContent value="brand">
-          <BrandCounter></BrandCounter>
-          <BrandForm></BrandForm>
-          <BrandTable></BrandTable>
-        </TabsContent>
-      </Tabs>
-    </>
-  );
-};
+    <Tabs defaultValue='vehicle'>
+      <TabsList>
+        <TabsTrigger value='vehicle'>Vehículos</TabsTrigger>
+        <TabsTrigger value='model'>Modelos</TabsTrigger>
+        <TabsTrigger value='brand'>Marcas</TabsTrigger>
+      </TabsList>
+      <TabsContent value='vehicle'>
+        <VehicleCounter></VehicleCounter>
+        <VehicleForm></VehicleForm>
+        <VehicleTable></VehicleTable>
+      </TabsContent>
+      <TabsContent value='model'>
+        <ModelCounter></ModelCounter>
+        <ModelForm></ModelForm>
+        <ModelTable></ModelTable>
+      </TabsContent>
+      <TabsContent value='brand'>
+        <BrandCounter></BrandCounter>
+        <BrandForm></BrandForm>
+        <BrandTable></BrandTable>
+      </TabsContent>
+    </Tabs>
+  )
+}
 
-export default VehiclesPage;
+export default VehiclesPage
